992f7696004b8fda7f8d0dca7b90ab8f5d6fca50
Joabe Lucena <joabelucena@gmail.com>
2021-04-08 02:31:29 -0300

    Changes: - Lambda for pre-processing included

--- etl/cleaned/main.tf
+++ etl/cleaned/main.tf
@@ -15,12 +15,47 @@ resource "aws_kinesis_firehose_delivery_stream" "cleaned_delivery_stream" {
   destination = "extended_s3"
 
   extended_s3_configuration {
-    role_arn            = var.iam_firehose_role_arn
-    bucket_arn          = aws_s3_bucket.cleaned_bucket.arn
+    role_arn   = var.iam_firehose_role_arn
+    bucket_arn = aws_s3_bucket.cleaned_bucket.arn
+
+    processing_configuration {
+      enabled = "true"
+
+      processors {
+        type = "Lambda"
+
+        parameters {
+          parameter_name  = "LambdaArn"
+          parameter_value = "${aws_lambda_function.lambda_cleaning.arn}:$LATEST"
+        }
+      }
+    }
   }
 
   kinesis_source_configuration {
-    role_arn            = var.iam_firehose_role_arn
-    kinesis_stream_arn  = var.kinesis_stream_arn
+    role_arn           = var.iam_firehose_role_arn
+    kinesis_stream_arn = var.kinesis_stream_arn
+  }
+
+}
+
+
+resource "aws_lambda_function" "lambda_cleaning" {
+  filename      = "ingestion/resources/lambda_function.zip"
+  function_name = "lambda_cleaning"
+  role          = var.iam_lambda_role_arn
+  handler       = "lambda_function.lambda_handler"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = filebase64sha256("ingestion/resources/lambda_function.zip")
+
+  runtime = "python3.6"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
   }
 }
